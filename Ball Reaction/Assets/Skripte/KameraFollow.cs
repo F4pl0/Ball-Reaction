635245150e5c824bffedb57c7e8e7066bca536ef
epixp3 <straka.filip@gmail.com>
2019-03-03 00:12:29 +0100

    Dodata skripta za smooth follow kamere

---
+++ Ball Reaction/Assets/Skripte/KameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KameraFollow : MonoBehaviour
+{
+
+    public GameObject objectToFollow;
+
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
